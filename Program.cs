70904f4992a9a56405624cbb48942fb9c03142b8
Home <Home@DESKTOP-C0B5N2B>
2023-06-28 13:56:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TestTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            if (name == "Вячеслав") Console.WriteLine("Привет, Вячеслав");
+            else Console.WriteLine("Нет такого имени");
+            Console.ReadLine();
+        }
+    }
+}
